fc6526f5cb59c50eef1a358f663c97a7c908fc85
YesicaLope <yesica.lope@tecsup.edu.pe>
2022-07-04 05:51:42 -0500

    colores

---
+++ XamEFCore/Interfaces/IConfigDataBase.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace XamEFCore.Interfaces
+{
+    public interface IConfigDataBase
+    {
+        string GetFullPath(string databaseFileName);
+    }
+
+}
